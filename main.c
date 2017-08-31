4765abbdf46c867488bd8e3cd2a4bb9231f7f1fc
Rinnegatamante <rinnegatamante@gmail.com>
2017-08-31 17:19:25 +0200

    Added sceIo redirection.
    Added sceIo redirection.

--- main.c
+++ main.c
@@ -6,8 +6,9 @@
 #include <libk/string.h>
 #include <kuio.h>
 
-#define HOOKS_NUM  6      // Hooked functions num
-#define BUF_SIZE   2048   // Savedata buffer size
+#define HOOKS_NUM   12     // Hooked functions num
+#define BUF_SIZE    2048   // Savedata buffer size
+#define MAX_HANDLES 16     // Maximum number of kuIo handles opened
 
 static uint8_t current_hook = 0;
 static SceUID hooks[HOOKS_NUM];
@@ -15,6 +16,8 @@ static tai_hook_ref_t refs[HOOKS_NUM];
 static char buffer[BUF_SIZE];
 static char titleid[16];
 static char fname[256];
+static uint8_t workslot = 0;
+static SceUID kuio_handles[MAX_HANDLES];
 
 static char log_file[64];
 void LOG(char* format, ...){
@@ -31,6 +34,51 @@ void LOG(char* format, ...){
 	kuIoWrite(fd, str, strlen(str));
 	kuIoWrite(fd, "\n", 1);
 	kuIoClose(fd);
+}
+
+void kuIoLongWrite(SceUID fd, uint8_t* buf, SceSize size){
+	int i = 0;
+	uint8_t* data = (uint8_t*)buf;
+	while (i < size){
+		int len = (i + BUF_SIZE > size) ? size - i : BUF_SIZE;
+		kuIoWrite(fd, &data[i], len);
+		i += len;
+	}
+}
+
+void kuIoLongRead(SceUID fd, uint8_t* buf, SceSize size){
+	int i = 0;
+	uint8_t* data = (uint8_t*)buf;
+	while (i < size){
+		int len = (i + BUF_SIZE > size) ? size - i : BUF_SIZE;
+		kuIoRead(fd, &data[i], len);
+		i += len;
+	}
+}
+
+void lockHandle(SceUID handle){
+	int i = 0;
+	while (kuio_handles[i] != 0){
+		i++;
+	}
+	kuio_handles[i] = handle;
+}
+
+void unlockHandle(SceUID handle){
+	int i = 0;
+	while (kuio_handles[i] != handle){
+		i++;
+	}
+	kuio_handles[i] = 0;
+}
+
+uint8_t checkHandle(SceUID handle){
+	int i = 0;
+	while (i < MAX_HANDLES){
+		if (handle == kuio_handles[i]) return 1;
+		i++;
+	}
+	return 0;
 }
 
 void hookFunction(uint32_t nid, const void *func){
@@ -50,13 +98,7 @@ int sceAppUtilSaveDataDataSave_patched(SceAppUtilSaveDataFileSlot* slot, SceAppU
 			case SCE_APPUTIL_SAVEDATA_DATA_SAVE_MODE_FILE:
 				kuIoOpen(fname, SCE_O_WRONLY | SCE_O_CREAT, &fd);
 				kuIoLseek(fd, files[fileIdx].offset, SEEK_SET);
-				int i = 0;
-				uint8_t* save_buf = (uint8_t*)files[fileIdx].buf;
-				while (i < files->bufSize){
-					int len = (i + BUF_SIZE > files[fileIdx].bufSize) ? files[fileIdx].bufSize - i : BUF_SIZE;
-					kuIoWrite(fd, &save_buf[i], len);
-					i += len;
-				}
+				kuIoLongWrite(fd, (uint8_t*)files[fileIdx].buf, files[fileIdx].bufSize);
 				kuIoClose(fd);
 				break;
 			default:
@@ -69,13 +111,7 @@ int sceAppUtilSaveDataDataSave_patched(SceAppUtilSaveDataFileSlot* slot, SceAppU
 	// Writing slot param file
 	sprintf(fname, "ux0:/data/savegames/%s/SLOT%u/SlotParam.bin", titleid, slot->id);
 	kuIoOpen(fname, SCE_O_WRONLY | SCE_O_CREAT, &fd);
-	int i = 0;
-	uint8_t* paramBuf = (uint8_t*)slot->slotParam;
-	while (i < sizeof(SceAppUtilSaveDataSlotParam)){
-		int len = (i + BUF_SIZE > sizeof(SceAppUtilSaveDataSlotParam)) ? sizeof(SceAppUtilSaveDataSlotParam) - i : BUF_SIZE;
-		kuIoWrite(fd, &paramBuf[i], len);
-		i += len;
-	}
+	kuIoLongWrite(fd, (uint8_t*)slot->slotParam, sizeof(SceAppUtilSaveDataSlotParam));
 	kuIoClose(fd);
 	
 	return 0;
@@ -90,18 +126,13 @@ int sceAppUtilSaveDataSlotGetParam_patched(unsigned int slotId, SceAppUtilSaveDa
 	kuIoOpen(fname, SCE_O_RDONLY, &fd);
 	if (fd < 0) return SCE_APPUTIL_ERROR_SAVEDATA_SLOT_NOT_FOUND;
 	else{
-		
-		// Reading slot param info
-		int i = 0;
-		uint8_t* paramBuf = (uint8_t*)param;
-		while (i < sizeof(SceAppUtilSaveDataSlotParam)){
-			int len = (i + BUF_SIZE > sizeof(SceAppUtilSaveDataSlotParam)) ? sizeof(SceAppUtilSaveDataSlotParam) - i : BUF_SIZE;
-			kuIoRead(fd, &paramBuf[i], len);
-			i += len;
-		}
-		kuIoClose(fd);
-		
+		kuIoLongRead(fd, (uint8_t*)param, sizeof(SceAppUtilSaveDataSlotParam));
+		kuIoClose(fd);	
 	}
+	
+	// Setting working slot
+	workslot = slotId;
+	
 	return 0;
 }
 
@@ -114,13 +145,7 @@ int sceAppUtilSaveDataSlotSetParam_patched(unsigned int slotId, SceAppUtilSaveDa
 	kuIoOpen(fname, SCE_O_WRONLY, &fd);
 	if (fd < 0) return SCE_APPUTIL_ERROR_SAVEDATA_SLOT_NOT_FOUND;
 	else{
-		int i = 0;
-		uint8_t* paramBuf = (uint8_t*)param;
-		while (i < sizeof(SceAppUtilSaveDataSlotParam)){
-			int len = (i + BUF_SIZE > sizeof(SceAppUtilSaveDataSlotParam)) ? sizeof(SceAppUtilSaveDataSlotParam) - i : BUF_SIZE;
-			kuIoWrite(fd, &paramBuf[i], len);
-			i += len;
-		}
+		kuIoLongWrite(fd, (uint8_t*)param, sizeof(SceAppUtilSaveDataSlotParam));
 		kuIoClose(fd);
 	}
 	
@@ -155,13 +180,7 @@ int sceAppUtilSaveDataSlotCreate_patched(unsigned int slotId, SceAppUtilSaveData
 	sprintf(fname, "ux0:/data/savegames/%s/SLOT%u/SlotParam.bin", titleid, slotId);
 	SceUID fd;
 	kuIoOpen(fname, SCE_O_WRONLY | SCE_O_CREAT, &fd);
-	int i = 0;
-	uint8_t* paramBuf = (uint8_t*)param;
-	while (i < sizeof(SceAppUtilSaveDataSlotParam)){
-		int len = (i + BUF_SIZE > sizeof(SceAppUtilSaveDataSlotParam)) ? sizeof(SceAppUtilSaveDataSlotParam) - i : BUF_SIZE;
-		kuIoWrite(fd, &paramBuf[i], len);
-		i += len;
-	}
+	kuIoLongWrite(fd, (uint8_t*)param, sizeof(SceAppUtilSaveDataSlotParam));
 	kuIoClose(fd);
 	
 	return 0;
@@ -175,6 +194,62 @@ int sceAppUtilSaveDataSlotDelete_patched(unsigned int slotId, SceAppUtilSaveData
 	kuIoRemove(fname);
 	
 	return 0;
+}
+
+SceUID sceIoOpen_patched(const char *file, int flags, SceMode mode) {
+	
+	SceUID ret = 0;
+	
+	// Attempting to access savedata, redirecting it
+	if (strncmp(file, "savedata0:", 10) == 0){
+		LOG("Redirecting %s via sceIoOpen", file);
+		sprintf(fname, "ux0:/data/savegames/%s/SLOT%d/%s", titleid, workslot, &file[10]);
+		kuIoOpen(fname, flags, &ret);
+		lockHandle(ret);
+	}else{
+		ret = TAI_CONTINUE(SceUID, refs[6], file, flags, mode);
+	}
+	
+	return ret;
+	
+}
+
+int sceIoClose_patched(SceUID fd) {	
+	if (checkHandle(fd)){
+		unlockHandle(fd);
+		kuIoClose(fd);
+	}else TAI_CONTINUE(int, refs[7], fd);
+	return 0;	
+}
+
+int sceIoWrite_patched(SceUID fd, const void *data, SceSize size) {	
+	if (checkHandle(fd)) kuIoLongWrite(fd, (uint8_t*)data, size);
+	else TAI_CONTINUE(int, refs[8], fd, data, size);
+	return size;
+}
+
+int sceIoRead_patched(SceUID fd, const void *data, SceSize size) {	
+	if (checkHandle(fd)) kuIoLongRead(fd, (uint8_t*)data, size);
+	else TAI_CONTINUE(int, refs[9], fd, data, size);
+	return size;
+}
+
+SceOff sceIoLseek_patched(SceUID fd, SceOff offset, int whence) {
+	SceOff ret;
+	if (checkHandle(fd)){
+		kuIoLseek(fd, offset, whence);
+		kuIoTell(fd, &ret);
+	}else ret =  TAI_CONTINUE(SceOff, refs[10], fd, offset, whence);
+	return ret;	
+}
+
+int sceIoLseek32_patched(SceUID fd, SceOff offset, int whence) {	
+	SceOff ret;
+	if (checkHandle(fd)){
+		kuIoLseek(fd, offset, whence);
+		kuIoTell(fd, &ret);
+	}else ret =  TAI_CONTINUE(SceOff, refs[11], fd, offset, whence);
+	return ret;
 }
 
 void _start() __attribute__ ((weak, alias ("module_start")));
@@ -199,6 +274,12 @@ int module_start(SceSize argc, const void *args) {
 	hookFunction(0xD1C6AB8E,sceAppUtilSaveDataDataRemove_patched);
 	hookFunction(0x7E8FE96A,sceAppUtilSaveDataSlotCreate_patched);
 	hookFunction(0x266A7646,sceAppUtilSaveDataSlotDelete_patched);
+	hookFunction(0x6C60AC61,sceIoOpen_patched);
+	hookFunction(0xC70B8886,sceIoClose_patched);
+	hookFunction(0x34EFD876,sceIoWrite_patched);
+	hookFunction(0xFDB32293,sceIoRead_patched);
+	hookFunction(0x99BA173E,sceIoLseek_patched);
+	hookFunction(0x49252B9B,sceIoLseek32_patched);
 	
 	return SCE_KERNEL_START_SUCCESS;
 }
